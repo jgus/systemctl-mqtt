abefb745fe2bd31587c29247e714a0d51e79e7f5
Josh Gustafson <j@gustafson.me>
2026-05-10 09:46:47 -0600

    kick action

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
       pkgs = nixpkgs.legacyPackages.${system};
       pythonPackages = pkgs.python3Packages;
 
-      version = "3.0.0";
+      version = "0.0.0";
     in
     {
       packages.default = pythonPackages.buildPythonApplication {
